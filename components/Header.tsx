94cf87b9213de595831105decf0ce9f2d5eed3cb
north-tower <muneshmyke@gmail.com>
2024-01-24 16:15:30 +0300

    5th

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,44 @@
 'use client';
 
 import Link from "next/link";
-import { useState } from "react";
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Fragment, useState } from "react";
+import { Bars3Icon , ChatBubbleLeftIcon, ChevronDownIcon, PaperAirplaneIcon, PhoneIcon, PlayCircleIcon, XMarkIcon} from "@heroicons/react/24/solid";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react"
+import { HomeIcon } from "lucide-react";
+
 
 function Header() {
 
     const [mobileMenuOpen , setMobileMenuOpen] = useState(false);
+
+    const products = [
+        {
+            name: "Book a Stay",
+            description: "Get a better understanding of your traffic",
+            href: "#",
+            icon: HomeIcon,
+
+        },
+        {
+            name: "Book a flight",
+            description: "Speak directly to your customers",
+            href: "#",
+            icon: PaperAirplaneIcon
+        },
+        {
+            name: "Contact our support team",
+            description: "Your customers' data will safe and secure",
+            href: "#",
+            icon: ChatBubbleLeftIcon
+        },
+    ];
+
+    const callsToAction = [
+        { name: "See Demo Booking", href: "#", icon: PlayCircleIcon }, {
+            name: "Contact Support", href: "#", icon: PhoneIcon
+        }
+    ]
+
 
   return (
     <header className="bg-[#013B94]">
@@ -27,7 +59,8 @@ function Header() {
             <div className="flex lg:hidden">
                 <button type="button" className="-m-2.5
                 inline-flex items-center justify-center 
-                rounded-md p-2.5 text-white" onClick={() => setMobileMenuOpen(true)}>
+                rounded-md p-2.5 text-white" 
+                onClick={() => setMobileMenuOpen(true)}>
                     <span className="sr-only">
                         Open main menu
 
@@ -37,6 +70,45 @@ function Header() {
 
                 </button>
             </div>
+
+            <Popover.Group className="hidden lg:flex lg:gap-x-12">
+                <Popover className="reletive">
+                    <Popover.Button className="flex items-center
+                    gap-x-1 text-sm font-semibold leading-6 text-white">
+                        Stays
+                        <ChevronDownIcon className="h-5 w-5 flex-none text-white"
+                        aria-hidden="true" />
+                    </Popover.Button>
+
+                    <Transition as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1" >
+
+                        <Popover.Panel className="absolute bg-white -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-1 ring-gray-900/5">
+                            <div className="p-4">
+                                {products.map((item) => (
+                                    <div key={item.name}
+                                    className="group relative flex items-center gap-x-6
+                                    rounded-lg p-4 
+                                    text-sm leading-6 
+                                    hover:bg-gray-50">
+                                       <div className="flex h-11 flex-none items-center justify-center rounded-lg bg-gray-50
+                                       group-hover:bg-gray-200">
+                                        <item.icon className="h-6 w-6 text-[#013B94] group-hover:text-blue-600" 
+                                        aria-hidden="true" />
+                                       
+                                        </div>
+                                        </div>
+                                ))}
+                            </div>
+                        </Popover.Panel>
+                    </Transition>
+                </Popover>
+            </Popover.Group>
         </nav>
     </header>
   )
