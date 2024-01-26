37eb7c28e91c5029fa8ec1bda5ca5ab7940e5f99
north-tower <muneshmyke@gmail.com>
2024-01-26 12:47:09 +0300

    9th

--- components/Header.tsx
+++ components/Header.tsx
@@ -109,19 +109,48 @@ function Header() {
                                        
                                         </div>
                                         <div className="flex-auto">
-                                        <a href={item.href} 
-                                        className="block 
-                                        font-semibold text-[#013B94]"> {item.name} <span className="absolute inset-0" />
-                                        </a>
-                                        <p className="mt-1 text-[#013B94]">{item.description}</p>
+                                            <a href={item.href} 
+                                            className="block 
+                                            font-semibold text-[#013B94]"> {item.name} <span className="absolute inset-0" />
+                                            </a>
+                                            <p className="mt-1 text-[#013B94]">{item.description}</p>
                                         </div>
                                         </div>
+                                ))}
+                            </div>
+
+                            <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-500">
+                                {callsToAction.map((item) => (
+                                    <a key={item.name}
+                                    href={item.href}
+                                    className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-[#013B94] hover:bg-gray-100">
+                                        <item.icon className="h-5 w-5 flex-none text-[#013B94]" aria-hidden="true" />
+                                        {item.name}
+                                    </a>
                                 ))}
                             </div>
                         </Popover.Panel>
                     </Transition>
                 </Popover>
+                <a href="#" className="text-sm font-semibold leading-6 text-white">
+                    Flights
+                </a>
+                <a href="#" className="text-sm font-semibold leading-6 text-white">
+                    Car Rentals
+                </a>
+                <a href="#" className="text-sm font-semibold leading-6 text-white">
+                    Attractions
+                </a>
+                <a href="#" className="text-sm font-semibold leading-6 text-white">
+                    Flight + Hotel
+                </a>
             </Popover.Group>
+
+            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                <a href="#" className="text-sm font-semibold leading-6 text-white">
+                    Log in <span aria-hidden="true">&rarr;</span>
+                </a>
+            </div>
         </nav>
     </header>
   )
