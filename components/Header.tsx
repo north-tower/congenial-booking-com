76731e3b6f78f2983e46634e9406926c2fb91f52
north-tower <muneshmyke@gmail.com>
2024-01-26 14:24:19 +0300

    10th

--- components/Header.tsx
+++ components/Header.tsx
@@ -152,6 +152,53 @@ function Header() {
                 </a>
             </div>
         </nav>
+
+        <Dialog as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}>
+            <div className="fixed inset-8 z-10" />
+
+            <Dialog.Panel className="fixed inset-y-0 right-8 z-10 w-full
+            overflow-y-auto bg-[#013B94] px-6 py-6 sm:max-w-sm sm:ring-1
+            sm:ring-gray-900/10">
+                <div className="flex items-center justify-between">
+                    <a href="#" className="-m-1.5 p-1.5">
+                        <span className="sr-only">Booking.com</span>
+                        <img className="h-8 w-auto" 
+                src="https://static1.squarespace.com/static/5bde0f00c3c16aa95581e2e2/62b4cb1add9d257dd43bb03d/62b653fedc7c895918d19b24/1656116254983/booking+logo+white.png?format=1500w"
+                        
+                        alt="" />
+                   
+                        </a>
+                        <button type="button" className="-m-2.5 
+                        rounded-md p-2.5 text-white"
+                        onClick={() => setMobileMenuOpen(false)} >
+                            <span className="sr-only">Close Menu</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+
+                        </button>
+                </div>                
+
+                <div className="mt-6 flow-root">
+                    <div className="-my-6 divide-y divide-gray-500/10">
+                        <div className="space-y-2 py-6">
+                            <Disclosure className="-mx-3" as="div" > 
+                                    {( {open} )  => (
+                                        <>
+                                            <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-white hover:bg-blue-800">
+                                                Stays
+                                                    <ChevronDownIcon className={cn ( open ? "rotate-180" : "", "h-5 w-5 flex-none")} />                                                <ChevronDownIcon className=""
+                                            </Disclosure.Button>
+                                        </>
+                                    )}
+                            </Disclosure>
+                        </div>
+                    </div>
+                </div>
+            </Dialog.Panel>
+                
+        </Dialog>
     </header>
   )
 }
