b7d7dde4bd6623bfa42682339c4166b321111fd5
north-tower <muneshmyke@gmail.com>
2024-01-27 14:17:27 +0300

    18th

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,12 @@
+import { fetchResults } from "@/lib/fetchResults";
+import { notFound } from "next/navigation";
+
+
 type Props = {
     searchParams: SearchParams;
 }
 
-type SearchParams = {
+export type SearchParams = {
     url: URL;
     group_adults: string;
     group_children: string;
@@ -12,8 +16,14 @@ type SearchParams = {
 };
 
 
-function SearchPage({ searchParams }: Props) {
-    console.log(searchParams);
+async function SearchPage({ searchParams }: Props) {
+    if (!searchParams.url) return notFound();
+
+    const results = await fetchResults(searchParams);
+
+    // if(!results) return <div>No results...</div>;
+
+
   return (
     <div>page</div>
   )
