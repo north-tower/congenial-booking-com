bc42a1b8064d2f8b4e201350839aff622a7427f9
north-tower <muneshmyke@gmail.com>
2024-01-27 16:10:52 +0300

    19th

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -21,7 +21,9 @@ async function SearchPage({ searchParams }: Props) {
 
     const results = await fetchResults(searchParams);
 
-    // if(!results) return <div>No results...</div>;
+    if(!results) return <div>No results...</div>;
+
+    console.log(results);
 
 
   return (
